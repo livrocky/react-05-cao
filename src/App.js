2cb359d912f5d98172ab18370edd92aa69478841
rokas <rokasra@gmail.com>
2022-06-03 16:59:55 +0300

    display not working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
+import TodoForm from './components/todoList';
 
 function App() {
   return (
     <div className='App'>
-      <form className='todoList container'>
-        <h1>You have 3 Todos</h1>
-        <p>
-          <div className='taskDisplay'>
-            task 1
-            <div className='x'>
-              <i class='fa fa-times-circle' aria-hidden='true'></i>
-            </div>
-          </div>
-        </p>
-        <p>task 2</p>
-        <p>task 3</p>
-        <div className='enterInput'>
-          <input type='text' name='task' className='task' placeholder='Enter item' />
-          <button className='submit-btn'>Submit</button>
-        </div>
-      </form>
+      <TodoForm />
     </div>
   );
 }
--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -1,23 +1,54 @@
 import { useState } from 'react';
 
-const [taskValue, setTaskValue] = useState;
+function TodoForm() {
+  const [taskValue, setTaskValue] = useState('');
+  const [showTask, setShowTask] = useState(false);
 
-function todoForm() {
-  <form className='todoList'>
-    <h1>You have 3 Todos</h1>
-    <div className='taskDisplay'>
-      <p>
-        task 1
-        <div className='x'>
-          <i class='fa fa-times-circle-o' aria-hidden='true'></i>
+  function inputEnterHandler(event) {
+    console.log('entering task to input');
+    const inputEl = event.target;
+    const ivestaReiksme = inputEl.value;
+    setTaskValue(ivestaReiksme);
+  }
+
+  return (
+    <form onSubmit={sendValues} className='todoList'>
+      <h1>You have 3 Todos</h1>
+      <div className='taskDisplay'>
+        <p>
+          <span className='x'>
+            {/* {taskValue} */}
+            <i className='fa fa-times-circle-o' aria-hidden='true'></i>
+          </span>
+        </p>
+      </div>
+
+      <div className='enterInputDisplay'>
+        <div className='inputTask'>
+          <input onChange={inputEnterHandler} value={taskValue} type='text' name='task' id='task' placeholder='Enter item' />
         </div>
-      </p>
-    </div>
-    <p>task 2</p>
-    <p>task 3</p>
-    <div className='enterInput'>
-      <input type='text' name='task' id='task' placeholder='Enter item' />
-      <button className='submit-btn'>Submit</button>
-    </div>
-  </form>;
+        <div className='inputBtn'>
+          <button onClick={sendValues} className='submit-btn'>
+            {' '}
+            Submit{' '}
+          </button>
+          {showTask && <p>{taskValue}</p>}
+        </div>
+      </div>
+    </form>
+  );
+
+  function sendValues(event) {
+    event.preventDefault();
+    const taskObj = { task: taskValue };
+    console.log('taskObj===', taskObj);
+
+    setTaskValue('');
+  }
+  //   function displayValue() {
+  //     console.log('displayValue called');
+  //     setTaskValue(!taskValue);
+  //   }
 }
+
+export default TodoForm;
